4c3dbd88b3d22f508473731731ec3836b55b0f03
CodingManoj <mvp2612@hotmail.com>
2023-04-25 22:38:47 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -8,3 +8,8 @@ module "redis" {
   source                 = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                    = var.ENV
 }
+
+module "mysql" {
+  source                 = "./vendor/modules/mysql"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                    = var.ENV
+}
