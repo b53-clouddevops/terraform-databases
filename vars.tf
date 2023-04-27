77950653c8c43179190a38d479e3f5a897234112
CodingManoj <mvp2612@hotmail.com>
2023-04-26 23:24:14 -0400

    Git Auto Push

--- vars.tf
+++ vars.tf
@@ -10,3 +10,9 @@ variable "MYSQL_RDS_INSTANCE_TYPE" {}
 variable "DOCDB_PORT" {}
 variable "DOCDB_INSTANCE_CLASS" {}
 variable "DOCDB_INSTANCE_COUNT" {}
+
+# Elasticcache Variables 
+variable "ELASTIC_CACHE_PORT" {}
+variable "ELASTIC_CACHE_NODE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_COUNT" {}
+variable "ELASTIC_CACHE_ENGINE_VERSION" {}
